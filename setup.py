7f1c4588387275f8f12a525444fe8ab2e9c02791
Pål Grønås Drange <PGDR@statoil.com>
2017-12-07 14:06:04 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pywhich',
+    version='1.0',
+    author='Software Innovation Bergen, Statoil ASA
+    author_email='fd_gpl@statoil.com',
+    description='pywhich --- which for Python modules',
+    scripts=['pywhich']
+)
